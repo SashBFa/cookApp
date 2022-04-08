9dc63aec580e3e03b0ab8ef7aa7b6dc73cfe56eb
SashBFa <bastienfajs@gmail.com>
2022-04-08 15:00:14 +0200

    search bar ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Receipts from "./components/Receipts";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="App">
-      <h1>React Cooking App</h1>
-      <input type="text" placeholder="Search..." />
-      <Receipts />
+      <h1 className="App__h1">React Cooking App</h1>
+      <input
+        className="App__searchBar"
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <Receipts search={searchTerm} />
     </div>
   );
 }
--- src/components/Receipts.js
+++ src/components/Receipts.js
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./Card";
 
-const Receipts = () => {
+const Receipts = ({ search }) => {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
-      .get("https://themealdb.com/api/json/v1/1/search.php?s=A")
+      .get("https://themealdb.com/api/json/v1/1/search.php?s=" + search)
       .then((element) => setData(element.data.meals));
-  }, []);
+  }, [search]);
+
   return (
     <ul className="receipt">
-      {data.map((meal) => (
-        <Card key={meal.idMeal} meal={meal} />
-      ))}
+      {data &&
+        data.slice(0, 24).map((meal) => <Card key={meal.idMeal} meal={meal} />)}
     </ul>
   );
 };
